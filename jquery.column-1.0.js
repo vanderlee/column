111848c482252ed39363c15e15fc6d005204f473
Martijn van der Lee <martijn@vanderlee.com>
2011-11-07 20:55:01 +0100

    Left CSS fix by GV.

--- jquery.column-1.0.js
+++ jquery.column-1.0.js
@@ -5,7 +5,7 @@
  *
  * Emulate CSS3 style column on browsers that don't support it.
  */
- 
+
 //TODO Split on hyphens, &shy;, and others.
 //TODO Configurable splitter, using regex patterns. ('syllables', 'words', 'sentences', 'punctuation', function)
 //TODO Add classes to different columns; 'column', 'last-column', 'first-column', use inner-div's so stuff can be layed out with CSS.
@@ -22,7 +22,7 @@
 if (String.prototype.indexOfRegExp == null) {
 	String.prototype.indexOfRegExp = function(pattern, modifiers) {
 		var re = new RegExp(pattern, modifiers);	// cached?
-		var m = re.exec(this.valueOf());	
+		var m = re.exec(this.valueOf());
 		return (m == null? -1 : m.index);
 	}
 }
@@ -36,14 +36,14 @@ if (String.prototype.indexOfRegExp == null) {
 			'rule_color':	'',
 			'rule_style':	'none',
 			'rule_width':	'medium',
-			'split':		'word'	
+			'split':		'word'
 		};
-		
+
 		var border_widths	= {	thin:	_measure_border_width('thin'),
 								medium: _measure_border_width('medium'),
 								thick:	_measure_border_width('thick')
-							};		
-		
+							};
+
 		function _measure_em(scope) {
 			var element = jQuery('<div style="display:none;height:10em;margin:0;padding:0;border:0;"></div>').appendTo(scope);
 			var px = element.height() / 10;
@@ -67,7 +67,7 @@ if (String.prototype.indexOfRegExp == null) {
 									node = node.splitText(split);
 								}
 							} while (split);
-							return contents;			
+							return contents;
 						},
 			sentence: 	function(node) {
 							var contents = new Array;
@@ -77,10 +77,10 @@ if (String.prototype.indexOfRegExp == null) {
 									node = node.splitText(split);
 								}
 							} while (split);
-							return contents;			
+							return contents;
 						}
 		}
-		
+
 		function _split(parent) {
 			var contents = new Array;
 			$(parent).contents().each( function(index, value) {
@@ -89,39 +89,39 @@ if (String.prototype.indexOfRegExp == null) {
 				} else {
 					contents.push(value);
 				}
-			});			
-			return contents;			
+			});
+			return contents;
 		}
-				
+
 		return this.each( function() {
 			// Merge options
-			if (options) { 
+			if (options) {
 				$.extend(settings, options);
-			}			
-			
+			}
+
 			// per-instance settings
 			var element			= this;
 			var content			= $(this).html();	// entire bulk
 			var contents		= _split(this);
-			var gap_normal		= _measure_em(this);						
-			
+			var gap_normal		= _measure_em(this);
+
 			// the active part
 			_resize();	// do once pre-load so we atleast have columns
 			$(window).resize(_resize).load(_resize);
-			
+
 			// worker
 			function _resize() {
 				// Clear columns
 				$(element).empty();
-			
+
 				var column_gap		= (settings.gap == parseFloat(settings.gap))? settings.gap : gap_normal;
-				
+
 				if (settings.rule_style != 'none') {
 					var rule_color = (settings.rule_color? settings.rule_color : $(element).css('color'));
-					var rule_width = (settings.rule_width == parseFloat(settings.rule_width)? settings.rule_width : border_widths[settings.rule_width]);				
+					var rule_width = (settings.rule_width == parseFloat(settings.rule_width)? settings.rule_width : border_widths[settings.rule_width]);
 					column_gap		-= rule_width;
-				}				
-				
+				}
+
 				if (settings.width != 'auto') {
 					var column_count = Math.floor(($(element).width() + rule_width) / (settings.width + column_gap));
 				} else if (settings.count != 'auto') {
@@ -129,15 +129,15 @@ if (String.prototype.indexOfRegExp == null) {
 				} else {
 					return;
 				}
-				
+
 				var width			= $(element).width() - ((column_count - 1) * column_gap);
 				var column_width	= Math.floor(width / column_count);
-																
+
 				// Setup columns
 				var left = 0;
 				for (var c = 0; c < column_count; ++c) {
 					var style	= 'position:absolute;'
-								+ (c > 0?				 'left:'+left+'px;' : '')
+								+ (c > 0?                'left:'+(left - Math.floor(column_gap / 2))+'px;' : '')
 								+ 'width:'+column_width+'px;'
 								+ (c > 0?				 'padding-left:'+Math.ceil(column_gap / 2)+'px;' : '')
 								+ (c < column_count - 1? 'padding-right:'+Math.floor(column_gap / 2)+'px;' : '')
@@ -150,33 +150,33 @@ if (String.prototype.indexOfRegExp == null) {
 						left	+= rule_width;
 					}
 					$(element).append('<div style="'+style+'"></div>');
-				}							
-				
+				}
+
 				// Determine height of total content in a single column
 				var first = $('div', element).first();
 				var height = first.html(content).height();
 				first.empty();
 				var height_step = Math.ceil(height / column_count);
-				
+
 				// Fill columns
 				var contents_length = contents.length;
 				var max_height = 0;
 				var i = 0;
 				for (var c = 0; c < column_count; ++c) {
 					var div = $('div', element).eq(c);
-					
+
 					if (c < column_count - 1) {
 						// detect overflow
 						while (i < contents_length && div.height() <= height_step) {
 							div.append(contents[i++]);
 						}
-						
+
 						// fill up rest of div
-						var div_height = div.height();					
+						var div_height = div.height();
 						while (i < contents_length && div.height() == div_height) {
 							div.append(contents[i++]);
 						}
-					
+
 						// retract last part of content
 						div.contents().last().remove();
 						--i;
@@ -186,14 +186,14 @@ if (String.prototype.indexOfRegExp == null) {
 							div.append(contents[i++]);
 						}
 					}
-					
+
 					max_height = Math.max(max_height, div.height());
 				}
 
 				// Set all to the same height
 				$('div', element).css('height', max_height);
 				$(element).css('height', max_height);
-			}		
+			}
 		});
 	};
 })( jQuery );
